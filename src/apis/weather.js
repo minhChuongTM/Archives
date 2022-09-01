0dcaf035dd097f34fe29d813526edf4c1a86416a
CHUONG\hient <minhchuongit76@gmail.com>
2022-09-01 11:18:43 +0700

    xong

--- src/apis/weather.js
+++ src/apis/weather.js
@@ -1,4 +1,7 @@
 import axios from 'axios'
 
-const API_KEY = '115dbed065611525b98943579998e5f1'
+const API_KEY = '350d9886ebbef5994e9a99f967bc4187'
 // https://api.openweathermap.org/data/2.5/weather?q=ho chi minh&appid=${API_KEY}&units=metric&lang=vi
+export const getAPI = (locationName) => {
+    return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${locationName}&appid=${API_KEY}&units=metric&lang=vi`)
+}
--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,22 +1,35 @@
 import { useEffect, useState } from 'react'
+import { getAPI } from '../../apis/weather'
+import Weather from '../../components/Weather'
 import './styles.css'
 
 const Welcome = () => {
   const [locationName, setLocationName] = useState('ho chi minh')
   const [weatherData, setWeatherData] = useState({})
 
+  console.log(getAPI)
   const onChange = e => {
     const value = e.target.value
     setLocationName(value)
   }
-
+  useEffect(() => {
+    getAPI(locationName).then(res => {
+      setWeatherData(res.data);
+    }).catch(error => {
+      console.log(error);
+    });
+  }, [locationName]);
   return (
     <section className='section'>
       <select className='form-select' value={locationName} onChange={onChange}>
         <option value='ho chi minh'>Ho Chi Minh</option>
         <option value='ha noi'>Ha Noi</option>
         <option value='da nang'>Da Nang</option>
+        <option value='quang ngai'>Quang Ngai</option>
       </select>
+      {weatherData.name &&(
+        <Weather data={weatherData} />
+      )}
     </section>
   )
 }
